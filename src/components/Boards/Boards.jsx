315634fdaeebd6afabf91020a8081a0e2c8d137a
afoobar <gr4k@bk.ru>
2021-03-15 20:50:03 +0300

    wokring on board

--- src/components/Boards/Boards.jsx
+++ src/components/Boards/Boards.jsx
@@ -1,136 +1,24 @@
 import React, { Component } from 'react';
 import { Container, Draggable } from 'react-smooth-dnd';
 import { Container as BootstapContainer } from 'react-bootstrap';
-import { applyDrag, generateItems } from "./utils";
 
-const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
-
-const columnNames = ['todo', 'in progress', 'done'];
-
-const cardColors = [
-  "azure",
-  "beige",
-  "bisque",
-  "blanchedalmond",
-  "burlywood",
-  "cornsilk",
-  "gainsboro",
-  "ghostwhite",
-  "ivory",
-  "khaki"
-];
-
-const data = require('../../data.json');
+const data = require('./data.json');
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Boards extends Component {
-  constructor() {
-    super();
-    this.onColumnDrop = this.onColumnDrop.bind(this);
-    this.onCardDrop = this.onCardDrop.bind(this);
-    this.getCardPayload = this.getCardPayload.bind(this);
-    this.state = {
-      // eslint-disable-next-line react/no-unused-state
-      scene: {
-        type: "container",
-        props: {
-          orientation: "horizontal"
-        },
-        children: generateItems(3, i => ({
-          id: `column${i}`,
-          type: "container",
-          name: columnNames[i],
-          props: {
-            orientation: "vertical",
-            className: "card-container"
-          },
-          children: generateItems(3, j => ({
-            type: "draggable",
-            id: `${i}${j}`,
-            props: {
-              className: "card",
-            },
-            data: lorem.slice(0, Math.floor(Math.random() * 150) + 30)
-          }))
-        }))
-      }
-    }
-  }
-
-  onColumnDrop(dropResult) {
-    // eslint-disable-next-line
-    const scene = Object.assign({}, this.state.scene);
-    scene.children = applyDrag(scene.children, dropResult);
-    this.setState({
-      scene
-    });
-  }
-
-  onCardDrop(columnId, dropResult) {
-    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
-      // eslint-disable-next-line
-      const scene = Object.assign({}, this.state.scene);
-      const column = scene.children.filter(p => p.id === columnId)[0];
-      const columnIndex = scene.children.indexOf(column);
-      // eslint-disable-next-line
-      const newColumn = Object.assign({}, column);
-      newColumn.children = applyDrag(newColumn.children, dropResult);
-      scene.children.splice(columnIndex, 1, newColumn);
-
-      this.setState({
-        scene
-      });
-    }
-  }
-
-  getCardPayload(columnId, index) {
-    // eslint-disable-next-line
-    return this.state.scene.children.filter(p => p.id === columnId)[0].children[
-      index
-    ];
-  }
-
   render() {
     return (
-      <BootstapContainer className="card-scene">
-        <Container
-          orientation="horizontal"
-          onDrop={this.onColumnDrop}
-          dragHandleSelector=".column-drag-handle"
-          dropPlaceholder={{
-            animationDuration: 150,
-            showOnTop: true,
-            className: 'cards-drop-preview'
-          }}
-        >
-          {this.state.scene.children.map(column => {
+      <BootstapContainer className="board-container">
+        <Container orientation="horizontal">
+          {data.boards[0].lanes.map(column => {
             return (
               <Draggable key={column.id}>
-                <div className={column.props.className}>
+                <div className="card-container">
                   <div className="card-column-header">
                     <span className="column-drag-handle">&#x2630;</span>
-                    {column.name}
+                    {column.title}
                   </div>
                   <Container
-                    {...column.props}
-                    groupName="col"
-                    onDragStart={e => console.log("drag started", e)}
-                    onDragEnd={e => console.log("drag end", e)}
-                    onDrop={e => this.onCardDrop(column.id, e)}
-                    getChildPayload={index =>
-                      this.getCardPayload(column.id, index)
-                    }
-                    dragClass="card-ghost"
-                    dropClass="card-ghost-drop"
-                    onDragEnter={() => {
-                      console.log("drag enter:", column.id);
-                    }}
-                    onDragLeave={() => {
-                      console.log("drag leave:", column.id);
-                    }}
-                    onDropReady={p => console.log('Drop ready: ', p)}
                     dropPlaceholder={{                      
                       animationDuration: 150,
                       showOnTop: true,
@@ -138,15 +26,16 @@ class Boards extends Component {
                     }}
                     dropPlaceholderAnimationDuration={200}
                   >
-                    {column.children.map(card => {
-                      return (
-                        <Draggable key={card.id}>
-                          <div {...card.props}>
-                            <p>{card.data}</p>
-                          </div>
-                        </Draggable>
-                      );
-                    })}
+                    <Draggable className="card">
+                      <div>
+                        <p>test</p>
+                      </div>
+                    </Draggable>
+                    <Draggable className="card">
+                      <div>
+                        <p>test</p>
+                      </div>
+                    </Draggable>
                   </Container>
                 </div>
               </Draggable>
