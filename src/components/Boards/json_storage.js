216aac88607d1a1e190a77e7fc1047a8a12da045
afoobar <gr4k@bk.ru>
2021-03-30 20:59:32 +0300

    small fix

--- src/components/Boards/json_storage.js
+++ src/components/Boards/json_storage.js
@@ -1,5 +1,5 @@
 export const getActiveDataAndDate = (data) => {
-  const activeBoard = data.boards.filter((b) => b.status == 'active')[0];
+  const activeBoard = data.boards.filter((b) => b.status === 'active')[0];
   return [activeBoard, activeBoard.date]
 }; 
 
@@ -7,6 +7,6 @@ export const getActiveDataAndDate = (data) => {
 export const setActiveDate = (date_str) => {
   const editJsonFile = require("edit-json-file");
   const file = editJsonFile(`${__dirname}/components/Boards/data.json`);
-  file.set("boards.[0].date", date_str);
+  file.append("boards.date", date_str);
   file.save();
 }
