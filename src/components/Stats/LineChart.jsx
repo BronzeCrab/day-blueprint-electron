a6f3b14a126283b281c53a5dc13c12aaa0957f08
afoobar <gr4k@bk.ru>
2021-04-02 17:24:09 +0300

    workin on linechart

--- src/components/Stats/LineChart.jsx
+++ src/components/Stats/LineChart.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2';
+import { asyncLocalStorage } from '../Boards/utils';
 
-// eslint-disable-next-line react/prefer-stateless-function
+const cards = await asyncLocalStorage.getItem('boards')
+console.log(cards);
+
 const data = {
   labels: ['1', '2', '3', '4', '5', '6'],
   datasets: [
