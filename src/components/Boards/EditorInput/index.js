d020f03e22595ff7c7a7315dca81801995720253
afoobar <gr4k@bk.ru>
2021-04-06 20:02:54 +0300

    added code btn to editor

--- src/components/Boards/EditorInput/index.js
+++ src/components/Boards/EditorInput/index.js
@@ -28,7 +28,7 @@ Editor.modules = {
   toolbar: [
     [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
     [{ size: [] }],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code'],
     [{ 'list': 'ordered' }, { 'list': 'bullet' },
     { 'indent': '-1' }, { 'indent': '+1' }],
     ['link', 'image', 'video'],
@@ -42,7 +42,7 @@ Editor.modules = {
 
 Editor.formats = [
   'header', 'font', 'size',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'bold', 'italic', 'underline', 'strike', 'blockquote', 'code',
   'list', 'bullet', 'indent',
   'link', 'image', 'video'
 ];
