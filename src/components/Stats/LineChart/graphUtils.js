f55fd4b68f78d8910791ea92d6fabd3d20d2c353
afoobar <gr4k@bk.ru>
2021-04-07 21:14:20 +0300

    refator identation

--- src/components/Stats/LineChart/graphUtils.js
+++ src/components/Stats/LineChart/graphUtils.js
@@ -3,71 +3,71 @@ import moment from 'moment';
 const dateFormat = 'YYYY-MM-DD'; 
 
 export const mockedData = {
-    labels: [],
-    datasets: [
-        {
-            label: 'Number of done cards per day',
-            data: [],
-            fill: false,
-            backgroundColor: 'rgb(60, 179, 113)',
-            borderColor: 'rgba(123, 239, 178, 1)',
-        },
-        {
-            label: 'Total number of cards per day',
-            data: [],
-            fill: false,
-            backgroundColor: '#003366',
-            borderColor: '#003366',
-        }
-    ],
+  labels: [],
+  datasets: [
+    {
+      label: 'Number of done cards per day',
+      data: [],
+      fill: false,
+      backgroundColor: 'rgb(60, 179, 113)',
+      borderColor: 'rgba(123, 239, 178, 1)',
+    },
+    {
+      label: 'Total number of cards per day',
+      data: [],
+      fill: false,
+      backgroundColor: '#003366',
+      borderColor: '#003366',
+    }
+  ],
 };
 
 export const options = {
-    scales: {
-        yAxes: [
-            {
-                ticks: {
-                    beginAtZero: true,
-                },
-            },
-        ],
-    },
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+            beginAtZero: true,
+        },
+      },
+    ],
+  },
 };
 
 export const getFormatedData = (storageCards, copiedData) => {
-    copiedData.labels = [];
-    copiedData.datasets[0].data = [];
-    const allCards = {};
-    for (let i = 0; i < 3; i += 1) {
-        Object.keys(storageCards.lanes[i].cards)
-            .forEach((key) => {
-                const cardArray = storageCards.lanes[i].cards[key]
-                cardArray.forEach((cardObj) => {
-                    cardObj.laneId = i;
-                    if (key in allCards) {
-                        allCards[key].push(cardObj);
-                    }
-                    else {
-                        allCards[key] = [cardObj];
-                    }
+  copiedData.labels = [];
+  copiedData.datasets[0].data = [];
+  const allCards = {};
+  for (let i = 0; i < 3; i += 1) {
+    Object.keys(storageCards.lanes[i].cards)
+      .forEach((key) => {
+        const cardArray = storageCards.lanes[i].cards[key]
+        cardArray.forEach((cardObj) => {
+            cardObj.laneId = i;
+            if (key in allCards) {
+                allCards[key].push(cardObj);
+            }
+            else {
+                allCards[key] = [cardObj];
+            }
 
-                });
-            });
+        });
+      });
     }
     Object.keys(allCards)
-        .sort((a, b) => moment(a, dateFormat).toDate() - moment(b, dateFormat).toDate())
-        .forEach((key) => {
-            copiedData.labels.push(key);
-            let numOfDone = 0;
-            const totalPerDay = allCards[key].length;
-            allCards[key].forEach((cardObj) => {
-                if (cardObj.laneId === 2) {
-                    numOfDone += 1;
-                }
-            });
-            copiedData.datasets[0].data.push(numOfDone);
-            copiedData.datasets[1].data.push(totalPerDay);
-        });
+      .sort((a, b) => moment(a, dateFormat).toDate() - moment(b, dateFormat).toDate())
+      .forEach((key) => {
+          copiedData.labels.push(key);
+          let numOfDone = 0;
+          const totalPerDay = allCards[key].length;
+          allCards[key].forEach((cardObj) => {
+              if (cardObj.laneId === 2) {
+                  numOfDone += 1;
+              }
+          });
+          copiedData.datasets[0].data.push(numOfDone);
+          copiedData.datasets[1].data.push(totalPerDay);
+      });
 
     return copiedData;
 };
