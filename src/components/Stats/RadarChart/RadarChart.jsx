51168c061d012a0662b338b2d49438847237a524
afoobar <gr4k@bk.ru>
2021-04-09 20:43:27 +0300

    adding radar chart

---
+++ src/components/Stats/RadarChart/RadarChart.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import React from 'react'
+import { Radar } from 'react-chartjs-2';
+
+import { asyncLocalStorage } from '../../Boards/utils';
+import {
+  mockedData,
+  options,
+  getFormatedData,
+} from './graphUtils';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class RadarChart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDataFromStorage();
+  };
+
+  fetchDataFromStorage = async () => {
+    try {
+      const storageCards = JSON.parse(await asyncLocalStorage.getItem('boards'));
+      const graphData = JSON.parse(JSON.stringify(mockedData));
+      const formatedData = getFormatedData(storageCards, graphData);
+
+      this.setState({ data: formatedData });
+      
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  render() {
+    const { data } = this.state;
+
+    if (!data)
+      return null;
+
+    return (<Radar data={data} options={options} />);
+  }
+}
+
+export default RadarChart
