4da9ae8a3cbfbe73d3d3e6ae8922e622935b2d40
afoobar <gr4k@bk.ru>
2021-04-11 20:56:57 +0300

    working on RadaChart

--- src/components/Stats/RadarChart/graphUtils.js
+++ src/components/Stats/RadarChart/graphUtils.js
@@ -18,20 +18,29 @@ export const options = {
 
 export const getFormatedData = (storageCards, copiedData) => {
     const allCards = {};
+    const allTags = {};
     for (let i = 0; i < 3; i+=1) {
     // eslint-disable-next-line @typescript-eslint/no-loop-func
     Object.keys(storageCards.lanes[i].cards).forEach(function(key) {
       const cardArray = storageCards.lanes[i].cards[key]
       cardArray.forEach((cardObj) => {
-          cardObj.laneId = i;
-          if (key in allCards) {
-              allCards[key].push(cardObj);
+        cardObj.laneId = i;
+        if (key in allCards) {
+          allCards[key].push(cardObj);
+        }
+        else {
+          allCards[key] = [cardObj];
+        }
+        cardObj.tags.forEach(function(tag) {
+          if (tag in allTags) {
+            allTags[tag] += 1;
           }
           else {
-              allCards[key] = [cardObj];
-          }
+            allTags[tag] = 1;
+          };
+        });
       });
-    })
+    });
   }
 
   let currMonth;
@@ -40,38 +49,33 @@ export const getFormatedData = (storageCards, copiedData) => {
   Object.keys(allCards)
     .sort((a, b) => moment(a, dateFormat).toDate() - moment(b, dateFormat).toDate())
     .forEach((key) => {
-        const newDate = new Date(key).getMonth();
-        if (currMonth === undefined || currMonth === newDate) {
-          currMonth = newDate;
+        const newDate = new Date(key);
+        const label = newDate.toISOString().slice(0, 7);
+        const newMonth = newDate.getMonth();
+        if (currMonth === undefined || currMonth === newMonth) {
+          currMonth = newMonth;
           obj = {
-            label: currMonth, 
+            label: label, 
             data: [],
             backgroundColor:'',
             borderColor: '',
             borderWidth: 1,
           }
-          for (let j = 0; j < allCards[key].length; j+=1) {
-            const cardObj = allCards[j];
+          allCards[key].forEach((cardObj) => {
             cardObj.tags.forEach((tag) => {
-              if (cardObj.tags.indexOf(tag) === -1) {
+              if (copiedData.labels.indexOf(tag) === -1) {
                 copiedData.labels.push(tag);
-                obj.data.push(1)
-              }
-              else {
-                obj.data[j] += 1
+                obj.data.push(allTags[tag])
               }
             });
-          };
-          obj.data = [1, 2, 3]
+          });
           const color = randomRgba();
           obj.backgroundColor = color + 0.2 + ')';
           obj.borderColor = color + 1 + ')';
         }
         else {
-          currMonth = newDate;
-          copiedData.datasets.push(obj)
-        }
 
+        }
         copiedData.datasets.push(obj)
     });
 
