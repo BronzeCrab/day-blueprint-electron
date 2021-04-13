ad2e2ee631a2612c0eac7651d108e4ad5b7c66e0
afoobar <gr4k@bk.ru>
2021-04-13 18:20:07 +0300

    workin on piechart

--- src/components/Stats/PieChart/graphUtils.js
+++ src/components/Stats/PieChart/graphUtils.js
@@ -1,26 +1,14 @@
 import { randomRgba } from '../VerticalBar/graphUtils';
 
 export const mockedData = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['To do', 'Doing', 'Done'],
   datasets: [
     {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      label: 'Number of cards with specific status',
+      data: [0, 0, 0],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
       ],
       borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
       ],
       borderWidth: 1,
     },
@@ -34,6 +22,19 @@ export const options = {
 }
 
 export const getFormatedData = (storageCards, copiedData) => {
+
+  for (let i = 0; i < 3; i+=1) {
+  // eslint-disable-next-line @typescript-eslint/no-loop-func
+    Object.keys(storageCards.lanes[i].cards).forEach(function(key) {
+      const cardArray = storageCards.lanes[i].cards[key]
+      cardArray.forEach(function(cardObj) {
+        copiedData.datasets[0].data[i] += 1;
+      });
+    });
+    const color = randomRgba();
+    copiedData.datasets[0].backgroundColor.push(`${color + 0.2  })`);
+    copiedData.datasets[0].borderColor.push(`${color + 1  })`);
+  };
 
   return copiedData;
 };
