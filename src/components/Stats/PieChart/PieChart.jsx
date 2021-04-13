cdea7d9b786e652b7f7fa2eb1c9e390411e7a9ed
afoobar <gr4k@bk.ru>
2021-04-13 17:33:33 +0300

    begin adding pie chart

---
+++ src/components/Stats/PieChart/PieChart.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import React from 'react'
+import { Pie  } from 'react-chartjs-2';
+
+import { asyncLocalStorage } from '../../Boards/utils';
+import {
+  mockedData,
+  options,
+  getFormatedData,
+} from './graphUtils';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class PieChart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDataFromStorage();
+  };
+
+  fetchDataFromStorage = async () => {
+    try {
+      const storageCards = JSON.parse(await asyncLocalStorage.getItem('boards'));
+      const graphData = JSON.parse(JSON.stringify(mockedData));
+      const formatedData = getFormatedData(storageCards, graphData);
+
+      this.setState({ data: formatedData });
+      
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  render() {
+    const { data } = this.state;
+
+    if (!data)
+      return null;
+
+    return (<Pie data={data} options={options} />);
+  }
+}
+
+export default PieChart
--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Navbar';
 import LineChart from './LineChart/LineChart';
 import VerticalBar from './VerticalBar/VerticalBar';
 import RadarChart from './RadarChart/RadarChart';
+import PieChart from './PieChart/PieChart';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Stats extends Component {
@@ -10,9 +11,14 @@ class Stats extends Component {
     return (
       <div className="stats-charts-wrapper">
         <Navbar />
+        <h3 className="chart-header">Number of done and all cards by days</h3>
         <LineChart />
+        <h3 className="chart-header">Number of cards with specific tag</h3>
         <VerticalBar />
+        <h3 className="chart-header">Number of cards with specific tag by months</h3>
         <RadarChart />
+        <h3 className="chart-header">Number of cards with specific status (do, doing or done)</h3>
+        <PieChart />
       </div>
     );
   }
